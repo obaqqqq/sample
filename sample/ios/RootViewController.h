0604a8ec416ac197a016a86610f364eec25cd458
Shinsuke Kasahara <obaqqqq@gmail.com>
2013-10-20 02:59:15 +0900

    Initial Commit

---
+++ sample/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  sampleAppController.h
+//  sample
+//
+//  Created by 笠原 新介 on 13/10/20.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ sample/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sample
+//
+//  Created by 笠原 新介 on 13/10/20.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
